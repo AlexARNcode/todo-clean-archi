917781998e5b0b21177ff491feb7c842ff6e85f7
Alex LENOVO <alex@dev.com>
2025-12-02 16:21:37 +0100

    Improve tests

--- src/domain/task/Task.spec.ts
+++ src/domain/task/Task.spec.ts
@@ -5,18 +5,20 @@ import { TaskId } from './TaskId'
 import { TaskStatus } from './TaskStatus'
 import { InvalidTaskTitleError, TaskTitle } from './TaskTitle';
 
+const VALID_TITLE = "Test task";
+
 describe("Task entity", () => {
   it("should create a task with id, title and default status TODO", () => {
     const id = new TaskId("1");
-    const task = new Task(id, TaskTitle.create("First task"));
+    const task = new Task(id, TaskTitle.create(VALID_TITLE));
 
     expect(task.id.value).toBe("1");
-    expect(task.title.value).toBe("First task");
+    expect(task.title.value).toBe(VALID_TITLE);
     expect(task.status).toBe(TaskStatus.TODO);
   });
 
   it('should allow marking a task as DONE', () => {
-    const task = new Task(new TaskId('1'), TaskTitle.create("First task"))
+    const task = new Task(new TaskId('1'), TaskTitle.create(VALID_TITLE))
     
     task.markAsDone()
 
@@ -24,7 +26,7 @@ describe("Task entity", () => {
   })
 
   it('should allow marking a task as TODO again', () => {
-    const task = new Task(new TaskId('1'), TaskTitle.create("First task"))
+    const task = new Task(new TaskId('1'), TaskTitle.create(VALID_TITLE))
     
     task.markAsDone()
     task.markAsTodo()
@@ -34,6 +36,11 @@ describe("Task entity", () => {
 });
 
 describe("TaskTitle Value Object", () => {
+  it("should create a valid title", () => {
+    const title = TaskTitle.create(VALID_TITLE);
+    expect(title.value).toBe(VALID_TITLE);
+});
+
   it("should not allow an empty title", () => {
        expect(() => TaskTitle.create("   ")).toThrow(InvalidTaskTitleError);
   })
